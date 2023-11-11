94c4969d1aff9d62cd5d149848a78ea0628019da
oiltema <art.lobanovskiy@gmail.com>
2023-11-11 17:18:48 +0300

    add array

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
+function shuffle(a) {
+    var j, x, i;
+    for (i = a.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = a[i];
+        a[i] = a[j];
+        a[j] = x;
+    }
+    return a;
+}
 
-// Ваш JavaScript-код здесь
-    function myFunction (id) {
 
+
+
+function myFunction (id) {
     const test_img = "/assets/images/default-cardLight.svg";
     const img = document.getElementById(id);
     img.src = test_img;
@@ -9,3 +20,7 @@
 
 }
 
+
+const start_pack = ["assets/style/bicycle.svg", "assets/style/flight-mode.svg", "assets/style/happy-house.svg", "assets/style/paper-ship.svg", "assets/style/rabbit.svg", "assets/style/rubber-duck.svg", "assets/style/teddy-bear.svg", "assets/style/truck.svg", "assets/style/bicycle.svg", "assets/style/flight-mode.svg", "assets/style/happy-house.svg", "assets/style/paper-ship.svg", "assets/style/rabbit.svg", "assets/style/rubber-duck.svg", "assets/style/teddy-bear.svg", "assets/style/truck.svg"];
+
+console.log(start_pack.length);
